b48cc3c1b4e0cf4b1c501a9d4a0923ded9c95a3a
PamuduW <pamuduwijesingha2k20@gmail.com>
2024-12-11 19:43:06 +0530

    test

--- Frontend/src/components/Simulation/SimData.tsx
+++ Frontend/src/components/Simulation/SimData.tsx
@@ -53,10 +53,10 @@ const SimData: React.FC<SimDataProps> = ({ eventId }) => {
      */
     useEffect(() => {
         if (userData) {
-            const socket = new WebSocket(`ws://localhost:8080/ws/integers`);
-            // const socket = new WebSocket(
-            //     `wss://ticketing---system-32a1f2f59169.herokuapp.com/ws/integers`
-            // );
+            // const socket = new WebSocket(`ws://localhost:8080/ws/integers`);
+            const socket = new WebSocket(
+                `wss://ticketing---system-32a1f2f59169.herokuapp.com/ws/integers`
+            );
 
             socket.onopen = () =>
                 console.log("WebSocket connection established");
--- Frontend/src/components/Simulation/SimLog.tsx
+++ Frontend/src/components/Simulation/SimLog.tsx
@@ -20,10 +20,10 @@ const SimLog: React.FC = () => {
      */
     useEffect(() => {
         if (userData) {
-            const socket = new WebSocket(`ws://localhost:8080/ws/text`);
-            // const socket = new WebSocket(
-            //     `wss://ticketing---system-32a1f2f59169.herokuapp.com/ws/text`
-            // );
+            // const socket = new WebSocket(`ws://localhost:8080/ws/text`);
+            const socket = new WebSocket(
+                `wss://ticketing---system-32a1f2f59169.herokuapp.com/ws/text`
+            );
 
             socket.onopen = () =>
                 console.log("WebSocket connection established");
